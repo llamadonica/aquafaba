54a5f70a5d4b9c0ef8423b16a65fc94911b7c7f4
Adam Stark <llamadonica@gmail.com>
2017-03-30 07:57:51 -0700

    Add a quick GUID v4 generator

---
+++ guid.js
@@ -0,0 +1,29 @@
+/**
+ * @license
+ * Copyright (c) 2017 Adam Stark. All rights reserved.
+ * This code may only be used under the BSD style license found at LICENSE.txt
+ */
+define('guid', [], function () {
+  let exports = {};
+  exports.guid = () => {
+    function s4() {
+      return Math.floor((1 + Math.random()) * 0x10000)
+      .toString(16)
+      .substring(1);
+    }
+    function s3() {
+      return Math.floor((1 + Math.random()) * 0x10000)
+      .toString(16)
+      .substring(2);
+    }
+    function s35() {
+      return (Math.floor((1 + Math.random()) * 0x10000) & 0x1bfff | 0x18000)
+      .toString(16)
+      .substring(1);
+    }
+
+    return s4() + s4() + '-' + s4() + '-4' + s3() + '-' +
+           s35() + '-' + s4() + s4() + s4();
+    }
+  }
+});
