25d6c31a0287999b9f87aa20142c0999e490cbc2
Adam Stark <llamadonica@gmail.com>
2017-06-05 09:00:08 -0700

    Fix for Symbol on IE11? (#8)

--- core.js
+++ core.js
@@ -32,7 +32,7 @@
       return `##Symbol#${this.str}#${this.ix}`
     }
   }
-  Symbol = Symbol || _SymbolPolyfill.make; // eslint-disable-line no-global-assign
+  window.Symbol = window.Symbol || _SymbolPolyfill.make; // eslint-disable-line no-global-assign
 })();
 (function(s,a,c,q){s[a]=s[a]||function(i,m,f){q.push([i,m,f,c._currentScript||
 c.currentScript]);};q=s[a].q=s[a].q||[];})(window,"define",document);
